64fae94914651bde467398c4543744add85663f0
Oz <ozengnr@gmail.com>
2022-04-24 12:39:31 -0400

    Update validate.py

--- validate.py
+++ validate.py
@@ -2,17 +2,17 @@
 import sqlalchemy as sql
 #get credentials
 engine = sql.create_engine =(
-    'snowflake://{user}:{password}@{account_identifier}/{database_name}/{schema_name}?warehouse=FIVETRAN_WAREHOUSE&role=ACCOUNTADMIN'.format(
-        user='oguner',
-        password='c2QbecJwfsKzYPGL',
-        role_name='ACCOUNTADMIN',
-        account_identifier='ms77939.us-east-2.aws',
-        warehouse_name='FIVETRAN_WAREHOUSE',
-        database_name='PI_ENGAGEMENT_DB',
-        schema_name='PENDO'
+    'snowflake://{user}:{password}@{account_identifier}/{database_name}/{schema_name}?warehouse=WAREHOUSE_NAME&role=ROLENAME'.format(
+        user='<username>',
+        password='<password>',
+        role_name='<ROLE_NAME>',
+        account_identifier='<account_identifier>',
+        warehouse_name='<WAREHOUSE_NAME>',
+        database_name='<DATABASE_NAME>',
+        schema_name='<SCHEMA_NAME>'
     )
 )
-
+#connect engine
 try:
     connection = engine.connect()
     results = engine.connect().execute('select current_version()').fetchone()
